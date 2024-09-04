ab9d41f43e962f35b63836b7e2682c8def74226f
abdo261 <abdellah.bs2018@gmail.com>
2024-09-04 14:48:58 +0100

    payment

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ io.on("connection", (socket) => {
 // require('./jobs/createMonthlyFees')
 // Start the server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
